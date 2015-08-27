7574400db7c0517f9209eb180f87c540370c8f14
apatriz <apatriz@users.noreply.github.com>
2015-08-27 15:52:26 -0400

    Fixed #1, and additional things
    - Made get_docs() more generalized. Is now a generator that generates full paths of files recursively
    - comp_lists() now removes the files if they are not found in the 'not drawn' list

--- lpdelete.py
+++ lpdelete.py
@@ -3,21 +3,19 @@
 
 import os
 from openpyxl import load_workbook
-filename = 'C:\\Macomb ROW\\Returned Docs Delivery Script\\test\\Armada_Not_Drawn.xlsx'
-lpdir = 'C:/Macomb ROW/Returned Docs Delivery Script/test/Armada Liber Page Docs' 
+filename = raw_input("Enter full path to spreadsheet file (include file extension): ")
+#'C:\\Macomb ROW\\Returned Docs Delivery Script\\test\\Armada_Not_Drawn.xlsx'
+lpdir = raw_input("Enter full path of directory containing the documents: ")
+#'C:/Macomb ROW/Returned Docs Delivery Script/test/Armada Liber Page Docs' 
 wb = load_workbook(filename, use_iterators=True)
 ws = wb.get_active_sheet()
 
-#Define function to create a list of all file names (without extension) in a directory (recursive).
-#The list output should contain all liber pages in the directory.
-#TODO: Would generator be more efficient?
-def get_lps(lpdir):
-    fnlist = []
+#Define generator to get all files (full paths) in a directory (recursive).
+def get_docs(lpdir):
     for path,dirs,files in os.walk(lpdir):
         for fn in files:
-            fnlist.append(fn[:fn.rfind('.')])
-    return fnlist    
-           
+            yield os.path.join(path,fn)
+
 #Define function that reads liber page values from a spreadsheet into a list (liber pages must be in column 'B'
 # and assumes row 1 is a header row and all liber pages are "Not Drawn")
 # TODO: Modify code to determine if liber page is 'Drawn' or 'Not Drawn', and then
@@ -27,7 +25,7 @@ def get_not_drawn(filename):
     hr = ws.get_highest_row()
     for i in range(2,hr):
         lp = ws.cell(row = i, column = 2).value
-        if lp != None:
+        if lp:
             ndlist.append(str(lp))
         else:
             return ndlist
@@ -37,9 +35,10 @@ def get_not_drawn(filename):
  # TODO: Modify to actually delete the 'drawn' liber pages from the directory.
  # Currently, it prints which liber pages should be deleted.
 def comp_lists(lpdir,filename):
-    for i in get_lps(lpdir):
-        if i not in get_not_drawn(filename):
-            print i, "should be deleted."
+    for i in list(get_docs(lpdir)):
+        if os.path.splitext(os.path.split(i)[1])[0] not in get_not_drawn(filename):
+            os.remove(i)
+            print i, "has been deleted."
 
 
 # Define generator object to recursively yield empty directory paths (deleting the "Drawn" liber pages will leave empty directories
@@ -50,10 +49,15 @@ def get_empty_dir(lpdir):
             yield path 
 
 # Define function to delete empty directories using generated list from GetEmptyDir
-#TODO: This function can be combined with generator object above. Not necessary to have seperate functions. Is generator neccessary??
+#TODO: Currently deletes empty directories at time of generation, but does not delete the empty directories
+# created after deleting the first "round" of empty directories.
 def del_empty_dir(lpdir):
     empty_dir = list(get_empty_dir(lpdir))
     for i in empty_dir:
         os.rmdir(i)
         print i, 'is empty and has been deleted.'
     print 'All empty directories have been deleted.'
+
+
+#print comp_lists(lpdir,filename)
+#print del_empty_dir(lpdir)
