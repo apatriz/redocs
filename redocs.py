74d06296e3619a36130271d68b60b47a874edb55
apatriz <apatriz@users.noreply.github.com>
2015-09-09 16:44:11 -0400

    Update redocs.py
    -added main()
    - added functionality to create_entry_list() to always populate the 'folder' value in the entry (uses the previous folder value if 'none'). first entry in entrylist will now always be a column header (set as global var)

--- redocs.py
+++ redocs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 #NAME: redocs.py
+##TODO: transform user input for column choice ("A","B",etc.) into corresponding integer val
+## --current functionality requires all parameter inputs are strings (this can be modified if not using raw_input()) to obtain parameter values
 
 """ This python script reads a spreadsheet column containing file names into a list,
     and compares it to a list of all files in a specific directory. The purpose is to identify files
@@ -13,10 +15,10 @@ from openpyxl import load_workbook
 from openpyxl import Workbook
 
 ##raw_input("Enter full path to spreadsheet file (include file extension): ")
-filename = 'C:/Macomb ROW/9_Sterling_Heights/Sterling_Heights_Not_Drawn.xlsx'
+filename = 'C:\\Macomb ROW\\script_testing\\redocs\\test\\Armada_Not_Drawn.xlsx'
 
 ##raw_input("Enter full path of directory containing the documents: ")
-lpdir = 'C:/Macomb ROW/9_Sterling_Heights/Sterling_Heights_Docs_Working'
+lpdir = 'C:\\Macomb ROW\\script_testing\\redocs\\test\\Armada Liber Page Docs'
 
 ##raw_input("Enter the spreadsheet column which lists the file names to be preserved (i.e. Enter '0' for column 'A', '1' for column 'B',etc.): ")
 fn_column = '1'
@@ -33,19 +35,21 @@ fold_col = '0'
 ##raw_input("Enter the spreadsheet column which lists the comments: ")
 comment_col = '2'
 
+##raw_input("Enter the column labels for the output spreadsheet (i.e. Folder,Liber Page,Comments for column 1, 2 and 3 labels respectively): ")
+col_header = 'Folder','Liber Page','Comments'
 
 wb = load_workbook(filename, use_iterators= True)
 ws = wb.get_active_sheet()
 
 
-#Define generator to get all files (full paths) in a directory (recursive).
+#define generator to get all files (full paths) in a directory (recursive).
 def get_docs(lpdir):
     for path,dirs,files in os.walk(lpdir):
         for fn in files:
             yield os.path.join(path,fn)
 
-#Define function that reads values from a spreadsheet into a list, based on user input
-#TODO: transform user input for column choice ("A","B",etc.) into corresponding integer val
+#define function that reads values from a spreadsheet into a list, based on user input
+
 def tokeep(filename):
     keeplist = []
     for row in ws.iter_rows():
@@ -54,8 +58,8 @@ def tokeep(filename):
     return keeplist
 
 
- # Define function that compares the two lists and deletes files that are not in 'tokeep' list
-def comp_lists(lpdir,filename):
+# define function that compares the two lists and deletes files that are not in 'tokeep' list
+def del_unwanted(lpdir,filename):
     for i in get_docs(lpdir):
         if os.path.splitext(os.path.split(i)[1])[0] not in tokeep(filename):
             # print i, "will be deleted"
@@ -64,15 +68,15 @@ def comp_lists(lpdir,filename):
     print "All unwanted files deleted."
 
 
-# Define generator object to recursively yield empty directory paths (deleting files may leave empty directories
+# define generator object to recursively yield empty directory paths (deleting files may leave empty directories
 # that should be cleaned up)
 def get_empty_dir(lpdir):
     for path,dirs,files in os.walk(lpdir):
         if not dirs and not files:
             yield path
 
-# Define function to delete empty directories using generated list from GetEmptyDir.
-# Will run until no empty directories are found.
+# define function to delete empty directories using generated list from GetEmptyDir.
+# will run until no empty directories are found.
 def cleanup(lpdir):
     while True:
         empty_dir = list(get_empty_dir(lpdir))
@@ -83,30 +87,43 @@ def cleanup(lpdir):
             print 'All empty directories deleted.'
             break
 
+# define function that returns a list of data entries for the preserved files. Each entry in the list is a tuple containing the needed data for each file (i.e.(folder, filename, comments)).
+# the first entry will always be the column header labels
 def create_entry_list(filename):
-    entrylist = []
+    entrylist = [(col_header)]
+    prevfol = ''
     for row in ws.iter_rows():
+        folder = row[int(fold_col)].value
+        fn = row[int(fn_column)].value
+        comment = row[int(comment_col)].value
+        if not folder:
+            folder = prevfol
+        else:
+            prevfol = folder
         if row[int(ident_column)].value == keep_val:
-            folder = row[int(fold_col)].value
-            fn = row[int(fn_column)].value
-            comment = row[int(comment_col)].value
             entry = folder,fn,comment
             entrylist.append(entry)
     return entrylist
 
+# define function that creates new workbook.xlsx named "Returned" inside the directory containing the preserved files,
+# which contains the data entries for the preserved files (from create_entry_list)
 def create_new_xl(filename):
     nb = Workbook(write_only=True)
     ns = nb.create_sheet()
-    el = create_entry_list(filename)
-    for row in el:
-        ns.append([i for i in row])
+    entrylist = create_entry_list(filename)
+    for entry in entrylist:
+        ns.append([value for value in entry])
     nb.save(lpdir + '/Returned.xlsx')
-    print "Saved new workbook to {0}/Returned.xlsx".format(lpdir)
+    print "Saved new spreadsheet containing preserved file entries to {0}/Returned.xlsx".format(lpdir)
 
 
-##print list(get_docs(lpdir))
-# print tokeep(filename)
-print comp_lists(lpdir,filename)
-print cleanup(lpdir)
-# print create_entry_list(filename)
-print create_new_xl(filename)
+# define the main function
+def main():
+    del_unwanted(lpdir,filename)
+    cleanup(lpdir)
+    create_new_xl(filename)
+
+# call the main function
+if __name__ == "__main__":
+    main()
+    
